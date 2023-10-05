d64fba5cd916a4547692329c5301b2c031283257
Philipe <dev.philipe@outlook.com>
2023-10-05 19:35:43 -0300

    test(netflix_shows): factory seems to reality

--- spec/factories/netflix_shows.rb
+++ spec/factories/netflix_shows.rb
@@ -1,15 +1,16 @@
 FactoryBot.define do
   factory :netflix_show do
-    show_type { "MyString" }
-    title { "MyString" }
-    director { "MyString" }
-    cast { "MyString" }
-    country { "MyString" }
-    date_added { "2023-10-04" }
-    release_year { 1 }
-    rating { "MyString" }
-    duration { "MyString" }
-    listed_in { "MyString" }
-    description { "MyText" }
+    show_id { "s64" }
+    show_type { "TV Show" }
+    title { "13 Reasons Why" }
+    director { "Daniel Stamm" }
+    cast { "Dylan Minnette, Katherine Langford, Kate Walsh, Derek Luke, Brian d'Arcy James, Alisha Boe, Christian Navarro, Miles Heizer, Ross Butler, Devin Druid, Michele Selene Ang, Steven Silver, Amy Hargreaves" }
+    country { "United States" }
+    date_added { "June 5, 2020" }
+    release_year { 2020 }
+    rating { "TV-MA" }
+    duration { "4 Seasons" }
+    listed_in { "Crime TV Shows, TV Dramas, TV Mysteries" }
+    description { "After a teenage girl's perplexing suicide, a classmate receives a series of tapes that unravel the mystery of her tragic choice." }
   end
 end
